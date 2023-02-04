c4a69c0148e62d8222eaaf7a06df768fd5975d84
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-02-04 19:23:15 +0530

    simple_group_chat_application

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let adminRoutes = require('./routes/admin');
-let shopRoutes= require('./routes/shop');
-
+// let adminRoutes = require('./routes/admin');
+// let shopRoutes= require('./routes/shop');
+const userRoutes = require("./routes/login"); // Create user route
+const homeRoutes = require("./routes/home");
 
 
 app.use(bodyParser.urlencoded({extended : false}));
 
+app.use(userRoutes); // *** Filtering Paths ***  // use user route
+app.use(homeRoutes);
 
-app.use('/admin',adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin',adminRoutes);
+// app.use(shopRoutes);
 
 app.use((req ,res,next)=>{
     res.status(404).send('<h1>Page not found</h1>');
--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,11 @@ const router = express.Router();
 
 
 router.get('/add-product',(req ,res ,next)=>{
-    console.log('In the middleware');
-    res.send(
-      '<form action="/add-product" method="POST"> <input type="text" name="title"> <button type="submit">Add Product</button></form>'
-      )
- });
-
+  console.log('In the middleware');
+  res.send(
+    '<form action="/add-product" method="POST"> <input type="text" name="title"> <button type="submit">Add Product</button></form>'
+    )
+});
  router.post('/add-product',(req ,res ,next)=>{
    console.log(req.body);
    res.redirect('/');
