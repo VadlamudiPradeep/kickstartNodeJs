476aecf5b6a36be9d0340362294a3cbbc26b3682
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-02-04 17:50:35 +0530

    inro_to_middleware

---
+++ app.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/',(req ,res ,next)=>{
+   console.log('This always runs');
+ next();
+})
+
+app.use('/add-product',(req ,res ,next)=>{
+    console.log('In the middleware');
+    res.send('<form action="/product" method="POST"> <input type="text" name="title> <button type="submit">Add Product</button></form>')
+ });
+
+ app.post('/product',(req ,res ,next)=>{
+   console.log(req.body);
+   res.redirect('/');
+ });
+
+app.use('/',(req ,res ,next)=>{
+   console.log('In the middleware');
+   res.send('<h1>Welcome to my express js</h1>')
+ 
+})
+
+
+
+app.listen(3000);
