08ffc64259c08cc012006f8097bfb8918a230244
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-02-07 03:09:27 +0530

    Full_stack_Appointment_App

---
+++ routes/homeroutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+const homeController = require("../controllers/homecontrollers");
+
+router.get("/", homeController.getHome);
+
+router.post("/add", homeController.addData);
+
+router.get("/get-data", homeController.getData);
+
+router.delete("/delete-data/:id", homeController.deleteData);
+
+module.exports = router;
